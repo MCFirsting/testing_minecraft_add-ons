ad4e19787338061c62e6517cb41623f3993e910a
MCFirsting <86946978+MCFirsting@users.noreply.github.com>
2025-11-23 14:09:07 +0700

    test

--- Add-ons 1.21.110/BP/scripts/loot_api.js
+++ Add-ons 1.21.110/BP/scripts/loot_api.js
@@ -2,7 +2,8 @@ import { world, system } from "@minecraft/server"
 system.beforeEvents.startup.subscribe(ev => {
     ev.itemComponentRegistry.registerCustomComponent("demo:loot_api", {
         onUseOn: e => {
-            if (e.block) {//กำหนดให้ไอเทมใช่งานแบบของดรอป (getLootTableManager)
+            if (e.block) {
+                //กำหนดให้ไอเทมใช่งานแบบของดรอป (getLootTableManager)
                 const stacks = world.getLootTableManager().generateLootFromBlock(e.block, e.itemStack)
                 if (stacks) {
                     for (const stack of stacks) {
